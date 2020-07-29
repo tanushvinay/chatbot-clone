8f9b8fdfb61fbb4955f7178e1d95518d95576c2d
tanushvinay <tanush.vinay@gmail.com>
2020-07-29 07:13:47 -0700

    Added Chat TextArea

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Typography, Icon } from 'antd';
+import Chatbot from "./Chatbot/Chatbot";
 const { Title } = Typography;
+
 
 function App() {
   return (
@@ -9,7 +11,7 @@ function App() {
         <Title level={2} >CHAT BOT APP&nbsp;<Icon type="robot" /></Title>
       </div>
       <div style={{ display: 'flex', justifyContent: 'center' }}>
-       
+       <Chatbot/>
       </div>
     </div>
   )
